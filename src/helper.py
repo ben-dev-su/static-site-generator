2851268f5f164069ebf1e63caa691be88506640f
ben-dev-su <206861590+ben-dev-su@users.noreply.github.com>
2026-03-19 20:56:46 +0100

    feat(inline): add text_to_textnodes function
    Add text_to_textnodes wich will utilize our previous created function to
    take in a single markdown formatted string and output a list of
    TextNodes each with their own text type.

--- src/helper.py
+++ src/helper.py
@@ -1,6 +1,7 @@
 import re
 
 from textnode import TextType, TextNode
+from split_delimiter import split_nodes_delimiter
 
 
 def extract_markdown_images(text):
@@ -22,16 +23,17 @@ def split_nodes_image(old_nodes: list[TextNode] | None):
             continue
 
         if node.text_type != TextType.TEXT:
-            new_nodes.append(TextNode(node.text, node.text_type))
+            new_nodes.append(node)
             continue
 
         images = extract_markdown_images(node.text)
 
         if not images:
-            new_nodes.append(TextNode(node.text, node.text_type))
+            new_nodes.append(node)
             continue
 
         remaining_text = node.text
+        seq = []
         for i in range(len(images)):
             seq = remaining_text.split(f"![{images[i][0]}]({images[i][1]})", 1)
 
@@ -40,6 +42,9 @@ def split_nodes_image(old_nodes: list[TextNode] | None):
             new_nodes.append(TextNode(images[i][0], TextType.IMAGE, images[i][1]))
 
             remaining_text = seq[1]
+
+        if seq[1] != "":
+            new_nodes.append(TextNode(seq[1], TextType.TEXT))
 
     return new_nodes
 
@@ -55,15 +60,16 @@ def split_nodes_link(old_nodes):
             continue
 
         if node.text_type != TextType.TEXT:
-            new_nodes.append(TextNode(node.text, node.text_type))
+            new_nodes.append(node)
             continue
 
         links = extract_markdown_links(node.text)
 
         if not links:
-            new_nodes.append(TextNode(node.text, node.text_type))
+            new_nodes.append(node)
             continue
 
+        seq = []
         remaining_text = node.text
         for i in range(len(links)):
             seq = remaining_text.split(f"[{links[i][0]}]({links[i][1]})", 1)
@@ -74,4 +80,21 @@ def split_nodes_link(old_nodes):
 
             remaining_text = seq[1]
 
+        if seq[1] != "":
+            new_nodes.append(TextNode(seq[1], TextType.TEXT))
+
     return new_nodes
+
+
+def text_to_textnodes(text):
+    node = TextNode(text, TextType.TEXT)
+    nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
+
+
+# text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+# text_to_textnodes(text)
---
+++ src/test_text_to_textnodes.py
@@ -0,0 +1,90 @@
+import unittest
+
+from helper import text_to_textnodes
+from textnode import TextNode, TextType
+
+
+class TestTextToTextNodes(unittest.TestCase):
+    def test_text_to_textnodes(self):
+        self.maxDiff = None
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        nodes = text_to_textnodes(text)
+
+        valid_nodes = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("text", TextType.BOLD),
+            TextNode(" with an ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word and a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" and an ", TextType.TEXT),
+            TextNode(
+                "obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"
+            ),
+            TextNode(" and a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://boot.dev"),
+        ]
+
+        self.assertListEqual(valid_nodes, nodes)
+
+    def test_text_to_textnodes_empty(self):
+        nodes = text_to_textnodes("")
+
+        valid_nodes = []
+
+        self.assertListEqual(valid_nodes, nodes)
+
+    def test_text_to_texnodes_no_text_nodes(self):
+        text = "**text**_italic_`code block`![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg)[link](https://boot.dev)"
+        nodes = text_to_textnodes(text)
+
+        valid_nodes = [
+            TextNode("text", TextType.BOLD),
+            TextNode("italic", TextType.ITALIC),
+            TextNode("code block", TextType.CODE),
+            TextNode(
+                "obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"
+            ),
+            TextNode("link", TextType.LINK, "https://boot.dev"),
+        ]
+
+        self.assertListEqual(valid_nodes, nodes)
+
+    def test_text_to_texnodes_only_text(self):
+        text = "[value for value in iterable] [value if condition else condition for value in iterable]"
+        nodes = text_to_textnodes(text)
+
+        valid_nodes = [
+            TextNode(text, TextType.TEXT),
+        ]
+
+        self.assertListEqual(valid_nodes, nodes)
+
+    def test_text_to_textnodes_mixed_double(self):
+        self.maxDiff = None
+        text = "This is **text****text** with an _italic__italic_ word and a `code block``code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg)![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)[link](https://boot.dev)"
+        nodes = text_to_textnodes(text)
+
+        valid_nodes = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("text", TextType.BOLD),
+            TextNode("text", TextType.BOLD),
+            TextNode(" with an ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word and a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode("code block", TextType.CODE),
+            TextNode(" and an ", TextType.TEXT),
+            TextNode(
+                "obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"
+            ),
+            TextNode(
+                "obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"
+            ),
+            TextNode(" and a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://boot.dev"),
+            TextNode("link", TextType.LINK, "https://boot.dev"),
+        ]
+
+        self.assertListEqual(valid_nodes, nodes)

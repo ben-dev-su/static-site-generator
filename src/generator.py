77b452d74598b62e61337546b4fc586b42b9e1fa
ben-dev-su <206861590+ben-dev-su@users.noreply.github.com>
2026-03-28 17:01:21 +0100

    feat(website): add GitHub Pages deployment support
    Add basepath argument for GitHub Pages compatibility.
    All root "/" references in href/src attributes are now replaced with the basepath.
    Renamed output directory from public/ to docs/ and added build.sh for deployment.

--- src/generator.py
+++ src/generator.py
@@ -4,7 +4,7 @@ from inline_markdown import extract_title
 from markdown_blocks import markdown_to_html_node
 
 
-def generate_page_recursively(from_dir_path, template_path, dest_dir_path):
+def generate_page_recursively(from_dir_path, template_path, dest_dir_path, basepath):
     dir_content = os.listdir(from_dir_path)
     if not dir_content:
         return
@@ -14,12 +14,12 @@ def generate_page_recursively(from_dir_path, template_path, dest_dir_path):
         dest_path = os.path.join(dest_dir_path, element.replace(".md", ".html"))
         print(f" * {from_path} -> {dest_path}")
         if os.path.isfile(from_path):
-            generate_page(from_path, template_path, dest_path)
+            generate_page(from_path, template_path, dest_path, basepath)
         else:
-            generate_page_recursively(from_path, template_path, dest_path)
+            generate_page_recursively(from_path, template_path, dest_path, basepath)
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"* {from_path} {template_path} -> {dest_path}")
 
     if not os.path.exists(from_path):
@@ -39,6 +39,8 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown)
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html)
+    template = template.replace('href="/', f'href="{basepath}')
+    template = template.replace('src="/', f'src="{basepath}')
 
     create_dest(dest_path, template)
 
--- src/main.py
+++ src/main.py
@@ -1,46 +1,47 @@
 import os
 import shutil
+import sys
 
 from copystatic import copy_static_recursevly
 from generator import generate_page_recursively, generate_page
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 
 
 def main():
+    basepath = "/"
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
     if not os.path.exists(dir_path_static):
         raise Exception("Not static folder found")
 
+    print("=============================")
     print("Deleting public directory...")
+    print("=============================")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
 
+    print()
+
     os.mkdir(dir_path_public)
 
+    print("===========================================")
     print("Copying static files to public directory...")
+    print("===========================================")
     copy_static_recursevly(dir_path_static, dir_path_public)
 
-    generate_page_recursively(dir_path_content, "./template.html", dir_path_public)
+    print()
 
-    # generate_page("content/index.md", "./template.html", "public/index.html")
-    # generate_page(
-    #     "content/blog/glorfindel/index.md",
-    #     "./template.html",
-    #     "public/blog/glorfindel/index.html",
-    # )
-    # generate_page(
-    #     "content/blog/tom/index.md", "./template.html", "public/blog/tom/index.html"
-    # )
-    # generate_page(
-    #     "content/blog/majesty/index.md",
-    #     "./template.html",
-    #     "public/blog/majesty/index.html",
-    # )
-    # generate_page(
-    #     "content/contact/index.md", "./template.html", "public/contact/index.html"
-    # )
+    print("=====================")
+    print("Generating content...")
+    print("=====================")
+    generate_page_recursively(
+        dir_path_content, "./template.html", dir_path_public, basepath
+    )
+    print()
 
 
 if __name__ == "__main__":
